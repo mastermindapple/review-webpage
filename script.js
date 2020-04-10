b54b0338d7320a594116af84ec6c12ea4e778635
mastermindapple <56500380+mastermindapple@users.noreply.github.com>
2020-04-10 18:42:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,315 @@
+var teachingRating = undefined;
+var doubtingRating = undefined;
+var conceptingRating = undefined;
+var netingRating = undefined;
+
+function teachOne(){
+   teachingRating = 1;
+   console.log(teachingRating);
+   document.getElementById("teaching-display").innerHTML = (teachingRating+" out of 10 stars");
+}
+function teachTwo(){
+   teachingRating = 2;
+   console.log(teachingRating);
+   document.getElementById("teaching-display").innerHTML = (teachingRating+" out of 10 stars");
+
+}
+function teachThree(){
+   teachingRating = 3;
+   console.log(teachingRating);
+   document.getElementById("teaching-display").innerHTML = (teachingRating+" out of 10 stars");
+
+}
+
+function teachFour(){
+ teachingRating = 4;
+ console.log(teachingRating);
+ document.getElementById("teaching-display").innerHTML = (teachingRating+" out of 10 stars");
+
+}
+
+function teachFive(){
+ teachingRating = 5;
+ console.log(teachingRating);
+ document.getElementById("teaching-display").innerHTML = (teachingRating+" out of 10 stars");
+
+}
+
+function teachSix(){
+ teachingRating = 6;
+ console.log(teachingRating);
+ document.getElementById("teaching-display").innerHTML = (teachingRating+" out of 10 stars");
+
+}
+
+function teachSeven(){
+ teachingRating = 7;
+ console.log(teachingRating);
+ document.getElementById("teaching-display").innerHTML = (teachingRating+" out of 10 stars");
+
+}
+
+function teachEight(){
+teachingRating = 8;
+ console.log(teachingRating);
+ document.getElementById("teaching-display").innerHTML = (teachingRating+" out of 10 stars");
+
+}
+
+function teachNine(){
+teachingRating = 9;
+ console.log(teachingRating);
+ document.getElementById("teaching-display").innerHTML = (teachingRating+" out of 10 stars");
+
+}
+
+function teachTen(){
+ teachingRating = 10;
+ console.log(teachingRating);
+ document.getElementById("teaching-display").innerHTML = (teachingRating+" out of 10 stars");
+
+}
+
+
+function conceptOne(){
+   conceptingRating = 1;
+   console.log(conceptingRating);
+   document.getElementById("concepting-display").innerHTML = (conceptingRating+" out of 10 stars");
+}
+function conceptTwo(){
+   conceptingRating = 2;
+   console.log(conceptingRating);
+   document.getElementById("concepting-display").innerHTML = (conceptingRating+" out of 10 stars");
+
+}
+function conceptThree(){
+   conceptingRating = 3;
+   console.log(conceptingRating);
+   document.getElementById("concepting-display").innerHTML = (conceptingRating+" out of 10 stars");
+
+}
+
+function conceptFour(){
+ conceptingRating = 4;
+ console.log(conceptingRating);
+ document.getElementById("concepting-display").innerHTML = (conceptingRating+" out of 10 stars");
+
+}
+
+function conceptFive(){
+ conceptingRating = 5;
+ console.log(conceptingRating);
+ document.getElementById("concepting-display").innerHTML = (conceptingRating+" out of 10 stars");
+
+}
+
+function conceptSix(){
+ conceptingRating = 6;
+ console.log(conceptingRating);
+ document.getElementById("concepting-display").innerHTML = (conceptingRating+" out of 10 stars");
+
+}
+
+function conceptSeven(){
+ conceptingRating = 7;
+ console.log(conceptingRating);
+ document.getElementById("concepting-display").innerHTML = (conceptingRating+" out of 10 stars");
+
+}
+
+function conceptEight(){
+conceptingRating = 8;
+ console.log(conceptingRating);
+ document.getElementById("concepting-display").innerHTML = (conceptingRating+" out of 10 stars");
+
+}
+
+function conceptNine(){
+conceptingRating = 9;
+ console.log(conceptingRating);
+ document.getElementById("concepting-display").innerHTML = (conceptingRating+" out of 10 stars");
+
+}
+
+function conceptTen(){
+ conceptingRating = 10;
+ console.log(conceptingRating);
+ document.getElementById("concepting-display").innerHTML = (conceptingRating+" out of 10 stars");
+
+}
+
+
+function netOne(){
+    netingRating = 1;
+    console.log(netingRating);
+    document.getElementById("neting-display").innerHTML = (netingRating+" out of 10 stars");
+ }
+ function netTwo(){
+    netingRating = 2;
+    console.log(netingRating);
+    document.getElementById("neting-display").innerHTML = (netingRating+" out of 10 stars");
+ 
+ }
+ function netThree(){
+    netingRating = 3;
+    console.log(netingRating);
+    document.getElementById("neting-display").innerHTML = (netingRating+" out of 10 stars");
+ 
+ }
+ 
+ function netFour(){
+  netingRating = 4;
+  console.log(netingRating);
+  document.getElementById("neting-display").innerHTML = (netingRating+" out of 10 stars");
+ 
+ }
+ 
+ function netFive(){
+  netingRating = 5;
+  console.log(netingRating);
+  document.getElementById("neting-display").innerHTML = (netingRating+" out of 10 stars");
+ 
+ }
+ 
+ function netSix(){
+  netingRating = 6;
+  console.log(netingRating);
+  document.getElementById("neting-display").innerHTML = (netingRating+" out of 10 stars");
+ 
+ }
+ 
+ function netSeven(){
+  netingRating = 7;
+  console.log(netingRating);
+  document.getElementById("neting-display").innerHTML = (netingRating+" out of 10 stars");
+ 
+ }
+ 
+ function netEight(){
+ netingRating = 8;
+  console.log(netingRating);
+  document.getElementById("neting-display").innerHTML = (netingRating+" out of 10 stars");
+ 
+ }
+ 
+ function netNine(){
+ netingRating = 9;
+  console.log(netingRating);
+  document.getElementById("neting-display").innerHTML = (netingRating+" out of 10 stars");
+ 
+ }
+ 
+ function netTen(){
+  netingRating = 10;
+  console.log(netingRating);
+  document.getElementById("neting-display").innerHTML = (netingRating+" out of 10 stars");
+ 
+ }
+
+ function doubtOne(){
+   doubtingRating = 1;
+   console.log(doubtingRating);
+   document.getElementById("doubting-display").innerHTML = (doubtingRating+" out of 10 stars");
+}
+function doubtTwo(){
+   doubtingRating = 2;
+   console.log(doubtingRating);
+   document.getElementById("doubting-display").innerHTML = (doubtingRating+" out of 10 stars");
+
+}
+function doubtThree(){
+   doubtingRating = 3;
+   console.log(doubtingRating);
+   document.getElementById("doubting-display").innerHTML = (doubtingRating+" out of 10 stars");
+
+}
+
+function doubtFour(){
+ doubtingRating = 4;
+ console.log(doubtingRating);
+ document.getElementById("doubting-display").innerHTML = (doubtingRating+" out of 10 stars");
+
+}
+
+function doubtFive(){
+ doubtingRating = 5;
+ console.log(doubtingRating);
+ document.getElementById("doubting-display").innerHTML = (doubtingRating+" out of 10 stars");
+
+}
+
+function doubtSix(){
+ doubtingRating = 6;
+ console.log(doubtingRating);
+ document.getElementById("doubting-display").innerHTML = (doubtingRating+" out of 10 stars");
+
+}
+
+function doubtSeven(){
+ doubtingRating = 7;
+ console.log(doubtingRating);
+ document.getElementById("doubting-display").innerHTML = (doubtingRating+" out of 10 stars");
+
+}
+
+function doubtEight(){
+doubtingRating = 8;
+ console.log(doubtingRating);
+ document.getElementById("doubting-display").innerHTML = (doubtingRating+" out of 10 stars");
+
+}
+
+function doubtNine(){
+doubtingRating = 9;
+ console.log(doubtingRating);
+ document.getElementById("doubting-display").innerHTML = (doubtingRating+" out of 10 stars");
+
+}
+
+function doubtTen(){
+ doubtingRating = 10;
+ console.log(doubtingRating);
+ document.getElementById("doubting-display").innerHTML = (doubtingRating+" out of 10 stars");
+
+}
+
+
+  
+
+   var btn = document.getElementById("btn");
+    
+    btn.addEventListener('click',(e) =>{
+        e.preventDefault();
+
+        var name = document.getElementById("name").value;
+        var suggestions = document.getElementById("suggestions").value;
+        var database = firebase.database();
+
+        if(!name || !teachingRating || !netingRating || !doubtingRating || !conceptingRating){
+         alert("Fill in all the values");
+         }
+
+         else{
+
+        database.ref('/users/'+name).set({
+            explanation_review: teachingRating,
+            doubts_review: doubtingRating,
+            connectivity_review: netingRating,
+            concept_clarity_review: conceptingRating,
+            more_suggestions: suggestions
+        });
+        document.getElementById("heading").innerHTML = ("Review Submitted!");
+        document.getElementById("txt").innerHTML = ("");
+        document.getElementById("sub").innerHTML = ("");
+        document.getElementById("teaching-head").innerHTML = ("");
+        document.getElementById("explain").innerHTML = ("");
+        document.getElementById("doubt").innerHTML = ("");
+        document.getElementById("concept").innerHTML = ("");
+        document.getElementById("connectivity-head").innerHTML = ("");
+        document.getElementById("smoothness").innerHTML = ("");
+        document.getElementById("sugg").innerHTML = ("");
+        document.getElementById("btn-head").innerHTML = ("");
+      }
+    });
+    
+    
